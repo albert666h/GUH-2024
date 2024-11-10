49358438df4a05e9e9843281008e2071346c9a14
Mariana Mass <mariana.mass@student.manchester.ac.uk>
2024-11-10 04:25:08 +0000

    quiz

--- quiz.py
+++ quiz.py
@@ -11,15 +11,14 @@ print the statement followed by a comma and space, then print True or False
 use stricktly the provided response template 
 
 response template:
-    [Statement1], [False]
-    [Statement2], [True]
-    [Statement3], [False]
+    [Statement1] -> [False]
+    [Statement2] -> [True]
+    [Statement3] -> [False]
     
 
 '''
 
 def gpt(data):
-    #prompt='Analyze this log of a WhatsApp chat of friends and based on it construct three statements for a fun quiz for the friends about their memories, two of which are false and only one is true'
     query = prompt + data
     messages=[
         {"role": "user", 
@@ -27,18 +26,32 @@ def gpt(data):
         }]
     chat = client.chat.completions.create(model='gpt-4-turbo', messages=messages)
     reply = chat.choices[0].message.content
-
+    return reply
     
-    print(f'ChatGPT: {reply}')
 
 def extractData(path):
     with ZipFile(path, 'r') as zObject: 
        zObject.extractall(path="chat/") 
-    #file= open('chat/_chat.txt')
     chat=''
     with open('chat/_chat.txt') as f:
         lines = f.readlines()
     chat=''.join(lines[-min(1500, len(lines)):])
     return gpt(chat)
+datum=[]
+for _ in range(5):
+    question=extractData('chat.zip')
+    parts=question.split('\n')
+    data=[]
+    for part in parts:
+        temp=part.split('->')
+        if 'True' in temp[1]:
+            temp[1]='True'
+        if 'False' in temp[1]:
+            temp[1]='False'
+        data.append(temp)
+    datum.append(data)
+print(datum)
 
-extractData('chat.zip')
+
+
+
